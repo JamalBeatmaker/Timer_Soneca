1a6460c26c689c6529d6293b77c3588830d8a0c4
Jamal Beatmaker <89939812+JamalBeatmaker@users.noreply.github.com>
2023-05-10 16:25:22 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+let dormir = false
+let start = 0
+let time = 0
+input.onButtonPressed(Button.A, function () {
+    dormir = true
+    start = input.runningTime()
+})
+input.onButtonPressed(Button.AB, function () {
+    if (!(dormir)) {
+        basic.showNumber(Math.idiv(time, 1000))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (dormir) {
+        time += input.runningTime() - start
+    }
+    dormir = false
+})
+basic.forever(function () {
+    if (dormir) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    } else {
+        basic.showIcon(IconNames.Asleep)
+    }
+})
+basic.forever(function () {
+    if (input.runningTime() - start >= 15000) {
+        music.playMelody("B A C5 C D A E F ", 200)
+    } else {
+        music.stopAllSounds()
+    }
+})
